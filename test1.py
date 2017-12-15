3e367c608ce7b665e5eeef935163d781306025db
mohankumarkandavel <mohankumar.be.cse@gmail.com>
2017-12-15 12:43:59 +0000

    pass random command to server

--- test1.py
+++ test1.py
@@ -1,3 +1,4 @@
+import random
 import sys
 sys.path.insert(0, '/home/HypothesisTest/')
 
@@ -10,11 +11,20 @@ sock = HypoClient.SockConnect()
 @given(s=text())
 @example(s='')
 def test1(s):
-    HypoClient.SockSendAll(s.encode('utf-8'))
+    '''HypoClient.SockSendAll(s.encode('utf-8'))'''
     print("test1:" + s)
 
+def sendCommand():
+    selectCommand = ['create and commit once', 'create and commit twice', 'create and commit thrice']
+    randomChoice = random.choice(selectCommand)
+    print(randomChoice)
+    HypoClient.SockSendAll(randomChoice.encode('utf-8'))
+    
 if __name__ == '__main__':
-    test1()
+    '''test1()'''
+    sendCommand()
+
+
 
 
 '''HypoClient.SockSendAll(b"Hello\n")'''
